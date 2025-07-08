2f5c5f17ec22f49009b612de234c12469becfcfb
MBashov <galacticos.mb@gmail.com>
2025-07-08 17:34:17 +0300

    Adding components

--- Workshop/softuni-forum/src/app/app.ts
+++ Workshop/softuni-forum/src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./core/header/header";
+import { Footer } from "./core/footer/footer";
+import { Main } from "./main/main";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, Header, Footer, Main],
+    templateUrl: './app.html',
+    styleUrl: './app.css'
 })
-export class App {
-  protected title = 'softuni-forum';
+export class App implements OnInit {
+
+    constructor(private http: HttpClient) { }
+
+    ngOnInit(): void {
+        this.http.get('http://localhost:3000/api/posts').subscribe(p => console.log(p));
+    }
+
 }
