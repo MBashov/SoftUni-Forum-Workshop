81e11206cac75e06b9c7fe87dbb8792fda6a2be7
MBashov <galacticos.mb@gmail.com>
2025-07-08 19:54:20 +0300

    Show loader

--- Workshop/softuni-forum/src/app/main/posts-list/posts-list.ts
+++ Workshop/softuni-forum/src/app/main/posts-list/posts-list.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../api.service';
 import { Post } from '../../types/post';
+import { Loader } from '../../shared/loader/loader';
 
 @Component({
   selector: 'app-posts-list',
-  imports: [],
+  imports: [Loader],
   templateUrl: './posts-list.html',
   styleUrl: './posts-list.css'
 })
 export class PostsList implements OnInit{
   posts: Post[] = [];
-
+  isLoading: boolean = true;
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.apiService.getPosts(5).subscribe(posts => {
       this.posts = posts;
+      this.isLoading = false;
     });
   }
 }
--- Workshop/softuni-forum/src/app/main/themes-list/themes-list.ts
+++ Workshop/softuni-forum/src/app/main/themes-list/themes-list.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../api.service';
 import { Theme } from '../../types/theme';
+import { Loader } from '../../shared/loader/loader';
 
 @Component({
   selector: 'app-themes-list',
-  imports: [],
+  imports: [Loader],
   templateUrl: './themes-list.html',
   styleUrl: './themes-list.css'
 })
 export class ThemesList implements OnInit {
   themes: Theme[] = [];
+  isLoading: boolean = true;
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.apiService.getThemes().subscribe(themes => {
       this.themes = themes;
+      this.isLoading = false;
     });
   }
 }
